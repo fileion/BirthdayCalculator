d8b7117646cf0408da49234a31043ab8cafd5826
siam02 <siam.jaman02@gmail.com>
2024-09-29 18:27:34 +0600

    style added in custom stylesheet

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'base': "0 2px 10px 0 rgba(0, 0, 0, .1)",
+      },
+    },
   },
   plugins: [],
 }
